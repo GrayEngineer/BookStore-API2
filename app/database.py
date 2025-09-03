67ee1ec0dfb15eeb68ab15c381e0517eddfe67ac
GrayEngineer <azfar_asif@digimarkdevelopers.com>
2025-09-03 17:42:01 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bookstore.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_qdvMWCcBNY10@ep-aged-unit-a1xgv1rd-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False} )
+engine = create_engine( SQLALCHEMY_DATABASE_URL )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
